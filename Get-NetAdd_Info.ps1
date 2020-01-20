0250ddfae2b6ea8c6fd6be276baa0dc2333cb457
Joshua Kennedy <j_kennedy@oxfordproperties.com>
2020-01-20 14:09:39 -0500

    Create Get-NetAdd_Info.ps1

---
+++ Get-NetAdd_Info.ps1
@@ -0,0 +1,13 @@
+function Get-IPConfig{
+
+param ( $RemoteComputer="LocalHost",
+
+ $OnlyConnectedNetworkAdapters=$true
+
+   )
+
+   
+
+gwmi -Class Win32_NetworkAdapterConfiguration -ComputerName $RemoteComputer | Where { $_.IPEnabled -eq $OnlyConnectedNetworkAdapters } | Format-List @{ Label="Computer Name"; Expression= { $_.__SERVER }}, IPEnabled, Description, MACAddress, IPAddress, IPSubnet, DefaultIPGateway, DHCPEnabled, DHCPServer, @{ Label="DHCP Lease Expires"; Expression= { [dateTime]$_.DHCPLeaseExpires }}, @{ Label="DHCP Lease Obtained"; Expression= { [dateTime]$_.DHCPLeaseObtained }}
+
+} 
